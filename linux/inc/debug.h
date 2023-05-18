4494fcb06ae64e6cb53b80c38e139d7ebb78f427
pengym <pengym@nushinetech.com>
2023-05-18 14:33:29 +0800

    给linux debug.h添加简易函数版本

--- linux/inc/debug.h
+++ linux/inc/debug.h
@@ -47,15 +47,56 @@ extern void debug_exit(void);
 #define DBG_printf_raw(level,s,params...) 	 if(level<=dbg_level) debug_printf(0, s ,##params)
 #define DBG_set_dbg_Level(_level) 		 (dbg_level = (_level))
 
+
+/* ######################## 下面是简单写法 ####################### */
+/* 带自动回车的版本 */
+#define dbg_debugln(s,params...)        DBG_print(DBG_DEBUG, s, ##params)
+#define dbg_infoln(s,params...)         DBG_print(DBG_INFO, s, ##params)
+#define dbg_sysln(s,params...)          DBG_print(DBG_SYS, s, ##params)
+#define dbg_warnln(s,params...)         DBG_print(DBG_WARNING, s, ##params)
+#define dbg_errln(s,params...)          DBG_print(DBG_ERR, s, ##params)
+/* 带自动回车，且带函数定位 */
+#define dbg_debugfl(s,params...)        DBG_print_fl(DBG_DEBUG, s, ##params)
+#define dbg_infofl(s,params...)         DBG_print_fl(DBG_INFO, s, ##params)
+#define dbg_sysfl(s,params...)          DBG_print_fl(DBG_SYS, s, ##params)
+#define dbg_warnfl(s,params...)         DBG_print_fl(DBG_WARNING, s, ##params)
+#define dbg_errfl(s,params...)          DBG_print_fl(DBG_ERR, s, ##params)
+/* 不带回车 */
+#define dbg_debug(s,params...)          DBG_printf(DBG_DEBUG, s, ##params)
+#define dbg_info(s,params...)           DBG_printf(DBG_INFO, s, ##params)
+#define dbg_sys(s,params...)            DBG_printf(DBG_SYS, s, ##params)
+#define dbg_warn(s,params...)           DBG_printf(DBG_WARNING, s, ##params)
+#define dbg_err(s,params...)            DBG_printf(DBG_ERR, s, ##params)
+/* 打印内核的hex值 */
+#define dbg_debughex(buf,len)           DBG_print_hex(DBG_DEBUG, buf, len)
+#define dbg_infohex(buf,len)            DBG_print_hex(DBG_INFO, buf, len)
+#define dbg_syshex(buf,len)             DBG_print_hex(DBG_SYS, buf, len)
+#define dbg_warnhex(buf,len)            DBG_print_hex(DBG_WARNING, buf, len)
+#define dbg_errhex(buf,len)             DBG_print_hex(DBG_ERR, buf, len)
+/* 打印原始数据,其他版本都带由时间显示（开启DEBUG_USE_TIME宏时） */
+#define dbg_debugraw(s,params...)       DBG_printf_raw(DBG_DEBUG, s, ##params)
+#define dbg_inforaw(s,params...)        DBG_printf_raw(DBG_INFO, s, ##params)
+#define dbg_sysraw(s,params...)         DBG_printf_raw(DBG_SYS, s, ##params)
+#define dbg_warnraw(s,params...)        DBG_printf_raw(DBG_WARNING, s, ##params)
+#define dbg_errraw(s,params...)         DBG_printf_raw(DBG_ERR, s, ##params)
+
+
+
 /**
  * @brief 打印错误原因，且执行 action 语句
  */
-#define DBG_INIT_ERROR_EXEC(expression, action)  do{            \
+#define DBG_ERROR_EXEC(expression, label, action)  do{          \
     if(expression){                                             \
-        DBG_print_fl(DBG_ERR,"("#expression") execute {"#action"}"); \
+        DBG_print_fl(DBG_ERR,label ": ("#expression") execute {"#action"}"); \
         action;                                                 \
     }                                                           \
 }while(0)
+
+/**
+ * @brief 打印错误原因，且执行 action 语句
+ */
+#define DBG_INIT_ERROR_EXEC(expression, action) DBG_ERROR_EXEC(expression, "init", action)
+
 
 #ifdef __cplusplus
 #if __cplusplus
